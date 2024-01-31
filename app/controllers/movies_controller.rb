46969595027b910e1642d533605da7d5c6f09882
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-30 18:14:25 -0700

    passed first 2 tests for US3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,18 @@ class MoviesController < ApplicationController
     json = JSON.parse(response.body, symbolize_names: true)
     @movies = json[:results]
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    # @movie = User.find(params[:movie_id])
+    
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
+    end
+    response = conn.get("/3/movie/#{params[:movie_id]}")
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @movie = json
+    
+  end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "Movies Details Page", type: :feature do
     # - List the first 10 cast members (characters & actress/actors)
     # - Count of total reviews
     # - Each review's author and information
-    it "Should see a button to 'Create a Viewing Party'" do
+    it "Should see a button to 'Create a Viewing Party'", :vcr do
       visit "/users/#{@user1.id}/movies/238"
 
       expect(page).to have_button("Create a Viewing Party")
@@ -30,6 +30,29 @@ RSpec.describe "Movies Details Page", type: :feature do
       click_button("Create a Viewing Party")
 
       expect(current_path).to eq("/users/#{@user1.id}/movies/238/viewing_party/new")
+    end
+
+    it "Should see a button to return to the Discover Page", :vcr do
+      visit "/users/#{@user1.id}/movies/238"
+
+      expect(page).to have_button("Return To Discover Page")
+
+      click_button "Return To Discover Page"
+
+      expect(current_path).to eq("/users/#{@user1.id}/discover")
+    end
+
+    it "Should see movie title, vote average, runtime, genres, summary, list of 10 cast members, count of total reviews, each review's author and information", :vcr do
+      visit "/users/#{@user1.id}/movies/238"
+
+      expect(page).to have_content("Movie Title: The Godfather")
+      expect(page).to have_content("Vote Average: 8.707")
+      expect(page).to have_content("Runtime: 2 hours 55 minutes")
+      expect(page).to have_content("Genre: Drama, Crime")
+      expect(page).to have_content("Summary:")
+      expect(page).to have_content("First 10 Cast Members:")
+      expect(page).to have_content("Total Reviews:")
+      expect(page).to have_content("Reach Review's Author and Information:")
     end
   end
 end
